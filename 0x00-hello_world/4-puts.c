dbf21b5465ea4bc6f7b57561f3ce9438eeb3b530
Ukinyang <ukemeinyang09@gmail.com>
2022-09-09 15:52:46 -0700

    update script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the phrase "programming is like building a multilingual puzzle"
+ * main - print the phrase "Programming is like building a multilingual puzzle"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"); 
-	return (0); 
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
