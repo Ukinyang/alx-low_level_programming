4ea4b07d5175bc36bb07d3644d9de04c33feac37
Ukinyang <ukemeinyang09@gmail.com>
2022-09-15 16:20:34 -0700

    update script

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H                                            
-#define MAIN_H 
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-int_isupper(int c);
+int _putchar(char c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
